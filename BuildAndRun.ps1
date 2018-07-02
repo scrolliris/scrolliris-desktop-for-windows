22e4ec8c6312a98d5bf6d23b2301cf8b47554f51
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-07-02 09:43:29 +0900

    Update build script
    * Add-AppxPackage script to register
    * Add restore task

--- BuildAndRun.ps1
+++ BuildAndRun.ps1
@@ -7,6 +7,7 @@ $displayname = "Scrolliris Desktop for Windows"
 $packagename = "Scrolliris.Desktop.Windows"
 $configuration = "Debug"
 $log = "Trace"
+$version = "0.0.1.0"
 
 Write-Host ""
 Write-Host "Configuration: $configuration"
@@ -29,27 +30,37 @@ if ($platform -ne "x86" -and $platform -ne "x64" -and $platform -ne "ARM") {
 taskkill /im 'MSBuild.exe' /f
 taskkill /im "${displayname}.exe" /f
 
+$currentDir = (Get-Item ".\").FullName
+$packageDir = "${currentDir}\${projectname}\AppPackages"
+$scriptPath = "${projectname}_${version}_${configuration}_Test" + `
+  "\Add-AppDevPackage.ps1"
+
 
 # Clean (resources and cache etc.)
 wsl rm -f `
   "${projectname}/{bin,obj}/${platform}/${configuration}/${displayname}.exe"
 wsl rm -fr "${projectname}/{bin,obj}/${platform}/${configuration}/*"
 
-MSBuild.exe .\"${projectname}"\"${projectname}".csproj /t:Restore
 MSBuild.exe .\"${projectname}"\"${projectname}".csproj /t:Clean
 
-# Build
-$binDir = ".\${projectname}\bin\${platform}\${configuration}"
+rm "${packageDir}" -r -fo
 
+
+# Restore
+MSBuild.exe .\"${projectname}"\"${projectname}".csproj /t:Restore
+
+
+# Build
 MSBuild.exe .\"${projectname}"\"${projectname}".csproj /t:Build `
   /p:Configuration="${configuration}" `
   /p:Platform="${platform}" `
   /p:Log="${log}" `
-  /p:AppxBundlePlatforms="${platform}" `
   /p:AppxBundle=Always `
+  /p:AppxBundlePlatforms="${platform}" `
   /p:UapAppxPackageBuildMode=StoreUpload
 
 if ($lastexitcode -ne 0) {
+  Write-Host
   Write-Host "Build faild with status: ${lastexitcode}"
   exit
 }
@@ -57,17 +68,27 @@ if ($lastexitcode -ne 0) {
 
 # Register
 Write-Host ""
-Get-AppXPackage | findstr /i "${packagename}"
+Get-AppXPackage | findstr /i "${packagename}$"
 
-if ($lastexitcode -ne 0) {
-  Add-AppxPackage -Register "${binDir}\AppxManifest.xml"
+if ($lastexitcode -eq 0) {
+  Get-AppXPackage -Name "${packagename}" | Remove-AppxPackage
 }
 
+# https://docs.microsoft.com/en-us/previous-versions/windows/apps/hh454036(v=vs.140)#sideload-your-app-package
+PowerShell.exe -ExecutionPolicy ByPass -Command `
+  "& ${packageDir}\${scriptPath} -Force"
+
+Write-Host ""
+Get-AppXPackage | findstr /i "${packagename}$"
+if ($lastexitcode -ne 0) {
+  Write-Host
+  Write-Host "Registration faild with status: ${lastexitcode}"
+  exit
+}
 
 # Run
 Write-Host ""
-Write-Host "Application '${displayname}' is starting..." -NoNewLine
+Write-Host "Application '${displayname}' is starting..."
 
-Write-Host ""
 explorer.exe shell:AppsFolder\$(Get-AppXPackage -name "$packagename" | `
   select -expandproperty PackageFamilyName)!App
